689b8f375fb1c865ef030e39c4a92b08d4aacfa0
restu surya <restu.suryao05@gmail.com>
2026-02-20 11:45:31 +0700

    Tes branch Pertemuan1

---
+++ IntroTest/Pertemuan1/src/Titik.java
@@ -0,0 +1,49 @@
+/*Nama File : Titik.java
+*Deskripsi  : Berisi atribut dan method dalam class Titik
+*Pembuat    : Restu Surya
+*Tanggal    : 19 Februari 2026
+ */
+
+public class Titik {
+    /*********ATRIBUT*********/
+    double absis;
+    double ordinat;
+
+    /************METHOD************/
+    //Konstruktor untuk membuat titik (0,0)
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+    }
+
+    //Mengembalikan nilai absis
+    double getAbsis(){
+        return absis;
+    }
+
+    //Mengembalikan nilai ordinat
+    double getOrdinat(){
+        return ordinat;
+    }
+
+    //menggeser absis titik dengan nilai baru x
+    void setAbsis(double x){
+        absis = x;
+    }
+
+    //menggeser ordinat titik dengan nilai baru y
+    void setOrdinat(double y){
+        ordinat = y;
+    }
+
+    //menggeser nilai absis dan ordinat titik masing-masing sejauh x dan y
+    void geser(double x, double y){
+        absis = absis + x;
+        ordinat = ordinat + y;
+    }
+
+    //Mencetak koordinat titik 
+    void printTitik(){
+        System.out.println("Titik (" + absis + "," + ordinat + ")");
+    }
+}//end class titik
